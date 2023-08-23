c5eba4e737fc6436664734ff91f9ca69db0d825a
maheshvenkat123 <mahesh.venkat222@gmail.com>
2023-08-23 14:45:03 +0530

    first commit

---
+++ src/com/y4j/oops/mahesh/ApplicationMain.java
@@ -0,0 +1,17 @@
+package com.y4j.education.mahesh;
+
+public class ApplicationMain {
+	
+	public static void main(String[] args) {
+		
+		StudentDetails mahesh = new StudentDetails("Venkat", "Mahesh", "mahesh.venkat222@gmail.com", 9704647127L, (byte)30, 'M', 'A', 100000);
+		StudentDetails kranthi = new StudentDetails("Kranthi", "Kumar", "kranthikumar@gmail.com", 7381230942L, (byte)28, 'M', 'B', 200000);
+		StudentDetails khaja = new StudentDetails("mohammed", "Khaja", "mohdkhaja@gmail.com", 1234567890L, (byte)27, 'M', 'B', 300000);
+		
+		mahesh.getStudentDetails();
+		kranthi.getStudentDetails();
+		khaja.getStudentDetails();
+		
+	}
+
+}
