c8e33139c031086ee6332dd37c528d7ff92235a6
Kranthi42 <139331724+Kranthi42@users.noreply.github.com>
2023-08-24 12:59:23 +0530

    Add files via upload

---
+++ src/com/y4j/oops/ApplicationMain.java
@@ -0,0 +1,10 @@
+package com.y4j.oops.education;
+
+public class ApplicationMain {
+
+	public static void main(String[] args) {
+	 StudentDetails kranthi = new  StudentDetails( "Kranthi","Kumar"," vanamala.142@gmail.com",  (byte)28, 9676726211L,'A','M', 3000000L);
+        kranthi . studentDetails();
+	}
+
+}
