40c7fb66367c89cc51b8cf6c233cd12fda91d1f2
nagarjunfsd <141901276+nagarjunfsd@users.noreply.github.com>
2023-08-24 16:47:13 +0530

    commits

---
+++ practice/SalaryMain.java
@@ -0,0 +1,11 @@
+package com.y4j.practice;
+
+public class SalaryMain {
+
+	public static void main(String[] args) {
+		
+		
+		Employee munirathnam = new Employee("Muni", "Rathnam", "muni@gmail.com",  6088978877L, (byte)39, 'M', "VLSI", 500000L );
+		munirathnam.employeeDetails();
+}
+}
